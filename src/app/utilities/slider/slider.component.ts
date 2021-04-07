3865504ba60d20cf87e08c6e96d16ca7afc51e63
Xansher <draco128@o2.pl>
2021-04-07 15:05:33 +0200

    Configured arrow visibility  Please enter the commit message for your changes. Lines starting

--- src/app/utilities/slider/slider.component.ts
+++ src/app/utilities/slider/slider.component.ts
@@ -33,6 +33,10 @@ export class SliderComponent implements OnInit, AfterViewInit{
   @Input()
   count:number;
 
+  //Arrow display settings
+  leftArrowVisible:boolean=false;
+  rightArrowVisible:boolean= false;
+
   width:number;
 
 
@@ -40,29 +44,33 @@ export class SliderComponent implements OnInit, AfterViewInit{
   loadSubscription$: Subscription
   resizeObservable$: Observable<Event>
   resizeSubscription$: Subscription
+
   ngOnInit(): void {
-  
+
+    this.setSize();
+
     this.loadObservable$ = fromEvent(window, 'load')
     this.loadSubscription$ = this.loadObservable$.subscribe( evt => {
-      if (this.main
-        && this.main.nativeElement
-        && this.main.nativeElement.offsetWidth){
-          this.itemsPerPage= Math.floor(this.main.nativeElement.offsetWidth/200);
-          this.cardWidth= Math.floor((this.main.nativeElement.offsetWidth/this.itemsPerPage)-10.1);
-        }
-      
+      this.setSize();
     });
+
     this.resizeObservable$ = fromEvent(window, 'resize');
     this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
-      this.itemsPerPage= Math.floor(this.main.nativeElement.offsetWidth/200);
-      this.cardWidth = Math.floor((this.main.nativeElement.offsetWidth/this.itemsPerPage)-10.1);
-      this.translate=0;
-      //console.log(` items: ${this.itemsPerPage} tOTAL: ${this.main.nativeElement.offsetWidth} CARD:${this.cardWidth}`);
+
+      this.setSize();
+     
     });
   }
 
   ngAfterViewInit(): void {
+    
 
+   this.setSize();
+    
+    this.cdRef.detectChanges();
+  }
+
+  setSize(){
     if (this.main
       && this.main.nativeElement
       && this.main.nativeElement.offsetWidth) {
@@ -71,10 +79,13 @@ export class SliderComponent implements OnInit, AfterViewInit{
     if(this.sliderWidth){
       this.itemsPerPage= Math.floor(this.main.nativeElement.offsetWidth/200);
       this.cardWidth= Math.floor((this.main.nativeElement.offsetWidth/this.itemsPerPage)-10.1);
+      this.translate=0;
+      this.checkRightArrow();
+      this.checkLeftArrow();
+     
+    }
     
-    }  
-    
-    this.cdRef.detectChanges();
+    //this.cdRef.detectChanges();  
   }
   
 
@@ -84,6 +95,8 @@ export class SliderComponent implements OnInit, AfterViewInit{
     if(this.translate<0){
       this.translate+=step;
     }
+    this.checkLeftArrow();
+    this.checkRightArrow();
   }
   next() {
     var size= this.list.length;
@@ -91,11 +104,45 @@ export class SliderComponent implements OnInit, AfterViewInit{
     if(this.translate>(-1*(size-this.itemsPerPage)*step)){
       this.translate-=step;
     }
-    
+    this.checkRightArrow();
+    this.checkLeftArrow();
   }
   
   
-  
+  checkRightArrow(){
+    if(this.list && this.list.length>0){
+      var size= this.list.length;
+      var step= this.cardWidth+10;
+      if(this.translate>(-1*(size-this.itemsPerPage)*step)){
+        
+        this.rightArrowVisible=true;
+        return;
+      }
+    }
+    
+
+    this.rightArrowVisible=false;
+  }
+
+  checkLeftArrow(){
+    if(this.translate<0){
+      this.leftArrowVisible=true;
+      return;
+    }
+    this.leftArrowVisible=false;
+  }
+
+  getLastIndex(){
+    var size=this.list.length;
+    if(size<=this.itemsPerPage){
+      return 0;
+    }
+    if(size%this.itemsPerPage == 0){
+      return (size/this.itemsPerPage)-1;
+    }
+
+    return Math.floor(size/this.itemsPerPage);
+  }
 
 
 }
