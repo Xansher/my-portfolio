be475a8ec60aa987a0576a8b24c314a62fab8e09
Xansher <grzesiekaszlar@gmail.com>
2021-04-09 21:00:19 +0200

    Created experience section
    Created and stylised experience component

--- src/app/aboutme/experience/experience.component.ts
+++ src/app/aboutme/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { expDTO } from './experience.model';
 
 @Component({
   selector: 'app-experience',
@@ -8,6 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class ExperienceComponent implements OnInit {
 
   constructor() { }
+
+  list:expDTO[]=[{
+    startDate:'2020.11',
+    endDate:'2021.02',
+    position:'Fullstack Developer',
+    company: 'Company',
+    description: "Obowiązki: \n* Programowanie urządzenia do wysyłania danych  \n* Tworzenie serwera do odbioru danych (API, Backend) \n* Tworzenie strony internetowej do wizualizacji danych"
+  },
+  {
+    startDate:'2020.11',
+    endDate:'2021.02',
+    position:'Fullstack Developer',
+    company: 'Company',
+    description: "Obowiązki: \n* Programowanie urządzenia do wysyłania danych  \n* Tworzenie serwera do odbioru danych (API, Backend) \n* Tworzenie strony internetowej do wizualizacji danych"
+  }];
 
   ngOnInit(): void {
   }
