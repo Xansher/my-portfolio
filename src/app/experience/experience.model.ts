670189875c3fc824c2fd8686a4ba89e6bd54b0de
Xansher <grzesiekaszlar@gmail.com>
2021-04-12 16:31:20 +0200

    Moved from aboutme/experience

---
+++ src/app/experience/experience.model.ts
@@ -0,0 +1,9 @@
+export interface experienceDTO {
+    id:number;
+    startDate:Date;
+    endDate:Date;
+    position:string;
+    company:string;
+    englishDuties:string;
+    polishDuties:string;
+}
