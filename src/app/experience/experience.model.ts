2251dd1bda2657379c7934de0d31a500b797d50d
Xansher <grzesiekaszlar@gmail.com>
2021-04-12 20:27:52 +0200

    Added creation DTO

--- src/app/experience/experience.model.ts
+++ src/app/experience/experience.model.ts
@@ -7,3 +7,12 @@ export interface experienceDTO {
     englishDuties:string;
     polishDuties:string;
 }
+
+export interface experienceCreationDTO {
+    startDate:Date;
+    endDate:Date;
+    position:string;
+    company:string;
+    englishDuties:string;
+    polishDuties:string;
+}
