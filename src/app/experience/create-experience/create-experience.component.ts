dff4cf6bcba21d9b2cbb57c5634f31a198367883
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 14:59:42 +0200

    Created create experience component

--- src/app/experience/create-experience/create-experience.component.ts
+++ src/app/experience/create-experience/create-experience.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ExperienceService } from '../experience.service';
 
 @Component({
   selector: 'app-create-experience',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateExperienceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private experienceService:ExperienceService, private router: Router) { }
 
   ngOnInit(): void {
+
   }
 
+  saveChanges(exp){
+    this.experienceService.create(exp).subscribe( ()=>{
+      this.router.navigate(['/settings/aboutme/experience']);
+    } );
+
+  }
 }
