027f886865cf1de29c046cd9e51ca9928d30bb45
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 18:21:23 +0200

    List as input

--- src/app/aboutme/experience/experience.component.ts
+++ src/app/aboutme/experience/experience.component.ts
@@ -1,5 +1,5 @@
 import { formatDate } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { experienceDTO } from 'src/app/experience/experience.model';
 
 
@@ -7,22 +7,15 @@ import { experienceDTO } from 'src/app/experience/experience.model';
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
+  
 })
 export class ExperienceComponent implements OnInit {
 
   constructor() { }
 
   language;
-  list:experienceDTO[]=[{
-    id:1,
-    startDate: new Date(),
-    endDate: new Date(),
-    position:'Fullstack Developer',
-    company: 'Company',
-    englishDuties: "Obowiązki: \n* Programowanie urządzenia do wysyłania danych  \n* Tworzenie serwera do odbioru danych (API, Backend) \n* Tworzenie strony internetowej do wizualizacji danych",
-    polishDuties: "siema"
-  }
-  ];
+  @Input()
+  list:experienceDTO[];
 
   ngOnInit(): void {
     this.language = localStorage.getItem('lang');
