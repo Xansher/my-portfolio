5cee0796cd3b44ec23599d2e1fe95e0a158a3650
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 21:41:46 +0200

    Added funcionality

--- src/app/settings/aboutme-aboutme-settings/aboutme-aboutme-settings.component.ts
+++ src/app/settings/aboutme-aboutme-settings/aboutme-aboutme-settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AboutmeService } from './aboutme.service';
 
 @Component({
   selector: 'app-aboutme-aboutme-settings',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutmeAboutmeSettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private aboutmeService:AboutmeService) { }
+  
+  
+  model;
+  form:FormGroup;
 
   ngOnInit(): void {
+    this.form=this.formBuilder.group({
+      englishDescription: '',
+      polishDescription: '',
+      photo: ''
+    });
+    this.aboutmeService.get().subscribe( about => {
+      this.model=about;
+      if(this.model !== undefined){
+        this.form.patchValue(this.model);
+      }
+    });
+    
+  }
+
+  changeEnglishMarkdown(e){
+    this.form.get('englishDescription').setValue(e);
+  }
+  changePolishMarkdown(e){
+    this.form.get('polishDescription').setValue(e);
+  }
+
+  onImageSelected(photo){  
+    this.form.get('photo').setValue(photo);
+
+  }
+  saveChanges(){
+      
+      this.aboutmeService.edit(this.form.value).subscribe(()=> {
+
+      });
   }
 
 }
