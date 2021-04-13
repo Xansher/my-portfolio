fb00b94b5c381d2b15b86bca0b735f367beceec1
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 21:39:23 +0200

    Created about me models

---
+++ src/app/settings/aboutme-aboutme-settings/aboutme.model.ts
@@ -0,0 +1,10 @@
+export interface aboutMeDTO{
+    englishDescription:string;
+    polishDescription:string;
+    photo:string;
+}
+export interface aboutMeCreatingDTO{
+    englishDescription:string;
+    polishDescription:string;
+    photo:File;
+}
