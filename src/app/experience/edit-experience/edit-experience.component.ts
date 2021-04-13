080bcf0528b45b2f4a68633b71f8dfcbd475309a
Xansher <grzesiekaszlar@gmail.com>
2021-04-13 18:19:16 +0200

    Created edit experience component

--- src/app/experience/edit-experience/edit-experience.component.ts
+++ src/app/experience/edit-experience/edit-experience.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Data, Router } from '@angular/router';
+import { experienceDTO } from '../experience.model';
+import { ExperienceService } from '../experience.service';
 
 @Component({
   selector: 'app-edit-experience',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditExperienceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute:ActivatedRoute, private experienceService:ExperienceService, private router: Router) { }
+
+  model:experienceDTO;
 
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe( (data: Data) => {
+      this.model= data['experience'];
+    });
+  }
+
+  saveChanges(data){
+    this.experienceService.edit(this.model.id, data).subscribe( ()=>{
+      this.router.navigate(['/settings/aboutme/experience']);
+    });
   }
 
 }
