e7c1c298c1799c190e3dfee93bf71298a15cdac5
Xansher <grzesiekaszlar@gmail.com>
2021-04-14 15:38:02 +0200

    Added polish langauage do footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { footerNav, Labels } from './footer.model';
 
 @Component({
   selector: 'app-footer',
@@ -9,7 +10,18 @@ export class FooterComponent implements OnInit {
 
   constructor() { }
 
+  footerNav:footerNav;
+  labels: Labels;
+
   ngOnInit(): void {
+    if(localStorage.getItem('lang')=="english"){
+      this.footerNav= {about: 'About me', contact: 'Contact', portfolio: 'Portfolio'};
+      this.labels= {explore: 'Explore', social: 'Social', contact: 'Contact', contactText: 'Fell free to contact me by email or page.'};
+    }
+    if(localStorage.getItem('lang')=="polish"){
+      this.footerNav= {about: 'O mnie', contact: 'Kontakt', portfolio: 'Portfolio'};
+      this.labels= {explore: 'Zobacz', social: 'Media', contact: 'Kontakt', contactText:'Kontaktuj się poprzez mail lub formularz.'};
+    }
   }
 
 }
