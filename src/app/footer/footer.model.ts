9b2f7a895637772c59678294e98f83b9923cdd5f
Xansher <grzesiekaszlar@gmail.com>
2021-04-14 15:32:56 +0200

    Created models for footer

---
+++ src/app/footer/footer.model.ts
@@ -0,0 +1,18 @@
+export interface footerNav{
+    about: string;
+    contact: string;
+    portfolio: string;
+}
+
+export interface Labels{
+    explore: string;
+    social: string;
+    contact: string;
+    contactText:string;
+}
+
+export interface ContactDTO {
+    linkedin: string,
+    github: string,
+    instagram
+}
