53284cedde75ff3ab49b2b6f26c950232b200d6b
Xansher <grzesiekaszlar@gmail.com>
2021-04-16 15:57:02 +0200

    Created template of contact page

---
+++ src/app/contact/index-contact/index-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-contact',
+  templateUrl: './index-contact.component.html',
+  styleUrls: ['./index-contact.component.css']
+})
+export class IndexContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
