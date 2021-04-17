2c90495d8ce86c6e3752da58f71edb0c9aa157b4
Xansher <grzesiekaszlar@gmail.com>
2021-04-17 14:01:37 +0200

    Added modal message with animation on send

--- src/app/contact/index-contact/index-contact.component.ts
+++ src/app/contact/index-contact/index-contact.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { formatCurrency } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { messageCreatingDTO } from '../contact.model';
@@ -7,7 +8,21 @@ import { FormContactComponent } from '../form-contact/form-contact.component';
 @Component({
   selector: 'app-index-contact',
   templateUrl: './index-contact.component.html',
-  styleUrls: ['./index-contact.component.css']
+  styleUrls: ['./index-contact.component.css'],
+  animations: [
+    trigger('modal', [
+      transition(
+        ":enter", [style({opacity:0}),
+        animate('0.2s ease-out', style({opacity: 1}))]
+        ),
+        transition(
+          ":leave", [style({opacity:1}),
+          animate('0.2s ease-out', style({opacity: 0}))]
+          )
+      ]
+      )
+    
+  ]
 })
 export class IndexContactComponent implements OnInit {
 
@@ -15,7 +30,7 @@ export class IndexContactComponent implements OnInit {
 
   @ViewChild('form')
   form:FormContactComponent;
-
+  modal:boolean=false;
 
   ngOnInit(): void {
   }
@@ -23,6 +38,7 @@ export class IndexContactComponent implements OnInit {
   send(messageCreatingDTO:messageCreatingDTO){
     this.contactService.create(messageCreatingDTO).subscribe(()=>{
       this.form.clearForm();
+      this.modal=true;
     });
   }
 
