bb4782402db755ab59d6133c7911bef978549fa7
Xansher <grzesiekaszlar@gmail.com>
2021-04-17 14:31:27 +0200

    Added polish language

--- src/app/contact/form-contact/form-contact.component.ts
+++ src/app/contact/form-contact/form-contact.component.ts
@@ -16,8 +16,24 @@ export class FormContactComponent implements OnInit {
   form:FormGroup;
   @Output()
   onSend= new EventEmitter<messageCreatingDTO>();
+  labels;
 
   ngOnInit(): void {
+    const lang=localStorage.getItem('lang');
+    if(lang=="english"){
+      this.labels= {
+        name: 'Your name', nameError: 'Name is required',
+        email: 'Your email', emailError:'Incorrect email', 
+        message: 'Message', messageError: 'Message is required'};
+    }
+    if(lang=="polish"){
+      this.labels= {
+        name: 'Twoje imię', nameError: 'Imię jest wymagane',
+        email: 'Twój email', emailError:'Nieprawidłowy email', 
+        message: 'Wiadomość', messageError: 'Wiadomość jest wymagana'};
+    }
+    
+
     this.form=this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.email, Validators.required]],
