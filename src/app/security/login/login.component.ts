299ad6bd731971f5b9edb8db6799bb168877ea00
Xansher <grzesiekaszlar@gmail.com>
2021-04-24 16:02:23 +0200

    Created login component

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { parseWebAPIErrors } from 'src/app/utilities/utils';
+import { userCredentials } from '../security.model';
+import { SecurityService } from '../security.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private securityService: SecurityService, private router: Router) { }
+
+  errors: string[] =[];
 
   ngOnInit(): void {
   }
 
+  login(userCredentials:userCredentials){
+    this.securityService.login(userCredentials).subscribe( response => { 
+      this.securityService.saveToken(response);
+      this.router.navigate(['/']);      
+    }, error => {
+      this.errors=parseWebAPIErrors(error);
+    });
+  }
 }
