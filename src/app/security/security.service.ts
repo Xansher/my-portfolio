2c1f5815f7a59fcd2883976412185f601e559811
Xansher <grzesiekaszlar@gmail.com>
2021-04-24 15:46:41 +0200

    Created security service

--- src/app/security/security.service.ts
+++ src/app/security/security.service.ts
@@ -1,20 +1,62 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { authenticationResponse, userCredentials } from './security.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SecurityService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  private apiURL= environment.apiURL+'/accounts';
+  private readonly tokenKey: string = 'token';
+  private readonly expirationTokenKey: string = 'token-expiration';
+  private readonly roleField ="role";
 
   isAuthenticated():boolean{
+    const token = localStorage.getItem(this.tokenKey);
+    if(!token){
+      return false;
+    }
+    const expiration = localStorage.getItem(this.expirationTokenKey);
+    const expirationDate = new Date(expiration);
+    if(expirationDate<= new Date()){
+      this.logout();
+      return false;
+    }
     return true;
   }
-  getRole(){
-    return 'admin';
+
+  getFieldFromJWT(field: string): string {
+    const token =localStorage.getItem(this.tokenKey);
+    if(!token){return '';}
+    const dataToken = JSON.parse(atob(token.split('.')[1]));
+    return dataToken[field];
   }
+
+  getRole(){
+    return this.getFieldFromJWT(this.roleField);
+  }
+
   logout(){
-    
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem(this.expirationTokenKey);
+  }
+
+  login(userCredentials: userCredentials):Observable<authenticationResponse>{
+    return this.http.post<authenticationResponse>(this.apiURL+'/login',userCredentials);  
+  }
+
+  register(userCredentials:userCredentials):Observable<authenticationResponse>{
+    return this.http.post<authenticationResponse>(this.apiURL+'/create', userCredentials);
+  }
+
+  saveToken(authenticationResponse:authenticationResponse){
+    localStorage.setItem(this.tokenKey, authenticationResponse.token);
+    localStorage.setItem(this.expirationTokenKey, authenticationResponse.expiration.toString());
   }
 
 }
