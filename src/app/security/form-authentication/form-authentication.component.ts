3b00f2b057e49fbb28a53b7905c1ec2dd067f1df
Xansher <grzesiekaszlar@gmail.com>
2021-04-24 14:31:14 +0200

    Created form authentication

--- src/app/security/form-authentication/form-authentication.component.ts
+++ src/app/security/form-authentication/form-authentication.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { userCredentials } from '../security.model';
+
 
 @Component({
   selector: 'app-form-authentication',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormAuthenticationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
+
+  form:FormGroup;
+  labels; 
+  @Input()
+  action;
+  @Output()
+  onSubmit= new EventEmitter<userCredentials>();
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+    const lang=localStorage.getItem('lang');
+    if(lang=="english"){
+      this.labels={email: 'Email', emailError: 'Email is required', password: 'Password',  passwordError: 'Password is required'};
+    }
+    if(lang=="polish"){
+      this.labels={email: 'Email', emailError: 'Email jest wymagany', password: 'Hasło', passwordError: 'Hasło jest wymagane'};
+    }
+  }
+
+  handleSubmit(){
+    this.onSubmit.emit(this.form.value);
   }
 
 }
